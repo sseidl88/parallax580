2b2b92c158f50a873bb1959553a6510f0b904302
sseidl88 <sseidl88@users.noreply.github.com>
2020-04-01 15:51:15 -0500

    for some reason i cant push changes from vs

---
+++ parallax-starter-master/ParallaxStarter/ISprite.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallaxStarter
+{
+    public interface ISprite
+    {
+        /// <summary>
+        /// Draws the ISprite.  This method should be invoked between calls to 
+        /// SpriteBatch.Begin() and SpriteBatch.End() with the supplied SpriteBatch
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch to draw with</param>
+        /// <param name="gameTime">The GameTime object</param>
+        void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+
+    }
+}
---
+++ parallax-starter-master/ParallaxStarter/StaticSprite.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallaxStarter
+{
+    public class StaticSprite : ISprite
+    {
+        // TODO: Implement class
+        public Vector2 position = Vector2.Zero;
+        Texture2D texture;
+
+        public StaticSprite(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+
+        public StaticSprite(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+
+    }
+}
